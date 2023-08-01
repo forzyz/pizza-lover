2ffec4f899af065920bc44d69cfba14f9c278e52
Ruslan <zimpuytel@gmail.com>
2023-08-01 12:24:01 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './scss/app.scss';
+
+import { Header } from './components/Header';
+import { Categories } from './components/Categories';
+import { Sort } from './components/Sort';
+import { PizzaBlock } from './components/PizzaBlock';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">All pizzas</h2>
+          <div className="content__items">
+            <PizzaBlock title="Mexican" price={20} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
